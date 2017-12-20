560aaa7d636051e2a1430699d62c9381417d215f
Pahmed Zillu <pahmed@Pahmeds-MBP.fios-router.home>
2017-12-20 01:38:44 -0500

    algorithm upgraded

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -19,11 +19,11 @@ public class Numbers {
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
-		int [] num = new int[100];
-		
+
+		int[] num = new int[100];
+
 		Random rand = new Random();
-		for(int i=0; i<num.length; i++){
+		for (int i = 0; i < num.length; i++) {
 			num[i] = rand.nextInt(1000);
 		}
 
@@ -32,35 +32,38 @@ public class Numbers {
 		Sort algo = new Sort();
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectDB.InsertDataFromArryToMySql(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectDB.readDataBase("selection_sort", "SortingNumbers");
-        for(String st:numbers){
-        	System.out.println(st);
-        }
+		System.out.println("Total Execution Time of " + num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
+		connectDB.InsertDataFromArryToMySql(num, "selection_sort", "SortingNumbers");
+		List<String> numbers = connectDB.readDataBase("selection_sort", "SortingNumbers");
+		for (String st : numbers) {
+			System.out.println(st);
+		}
+
 		int n = num.length;
-		randomize (num, n);
+		randomize(num, n);
 		//Insertion Sort
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
 
 		//Continue for rest of the Sorting Algorithm....
-
-	}
-
-	public static void randomize( int arr[], int n)
-	{
-		Random r = new Random();
-		// Start from the last element and swap one by one. We don't
-		// need to run for the first element that's why i > 0
-		for (int i = n-1; i > 0; i--) {
-			int j = r.nextInt(i);
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
+		connectDB.InsertDataFromArryToMySql(num, "insertion_sort", "SortingNumbers");
+		List<String> numbers1 = connectDB.readDataBase("insertion_sort", "SortingNumbers");
+		for (String st : numbers1) {
+			System.out.println(st);
 		}
+
+		public static void randomize ( int arr[], int n)
+		{
+			Random r = new Random();
+			// Start from the last element and swap one by one. We don't
+			// need to run for the first element that's why i > 0
+			for (int i = n - 1; i > 0; i--) {
+				int j = r.nextInt(i);
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
 	}
-
-
-}
